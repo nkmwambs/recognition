d8118ada0d11c55123838dc1f11a57eedcafb9ed
Onduso <londuso@us.ci.org>
2021-12-20 12:54:15 +0300

    Reworked the privilege.php code to add more if conditions to avoid the error when writing the privildge css

--- application/views/backend/privileges.php
+++ application/views/backend/privileges.php
@@ -1,47 +1,79 @@
 <style>
-<?php
+	<?php
 	$entitlements = $this->db->get("entitlement")->result_object();
-	
-	foreach($entitlements as $entitlement):
-	
-	echo ".".$entitlement->name."{display:none;}";
+	$priviledges_style='';
+	foreach ($entitlements as $entitlement) :
+
+		$priviledges_style .= "." . $entitlement->name . "{display:none;}";
 
 	endforeach;
 
-	foreach($entitlements as $entitlement):
-		if($this->crud_model->user_privilege($this->session->profile_id,$entitlement->name)){
+	foreach ($entitlements as $entitlement) :
+		if ($this->crud_model->user_privilege($this->session->profile_id, $entitlement->name)) {
 
-			echo ".".$entitlement->name."{display:block;}";
-	 	
-			
-			if($entitlement->derivative_id !== 0){
-				$first_parent = $this->db->get_where("entitlement",array("entitlement_id"=>$entitlement->derivative_id))->row();
-			
-				echo ".".$first_parent->name."{display:block;}";
-				
-				if($first_parent->derivative_id !== 0){
-						$second_parent = $this->db->get_where("entitlement",array("entitlement_id"=>$first_parent->derivative_id))->row();
-					
-						echo ".".$second_parent->name."{display:block;}";
-					
-						if($second_parent->derivative_id !== 0){
-							$third_parent = $this->db->get_where("entitlement",array("entitlement_id"=>$second_parent->derivative_id))->row();
-							
-								echo ".".$third_parent->name."{display:block;}";
-								
-								if($third_parent->derivative_id !== 0){
-									$fourth_parent = $this->db->get_where("entitlement",array("entitlement_id"=>$third_parent->derivative_id))->row();
-								
-										echo ".".$fourth_parent->name."{display:block;}";		
+			$priviledges_style .= "." . $entitlement->name . "{display:block;}";
+
+
+			if ($entitlement->derivative_id !== 0) {
+
+				$first_parent_obj = $this->db->get_where("entitlement", array("entitlement_id" => $entitlement->derivative_id));
+
+				if ($first_parent_obj->num_rows()>0) {
+
+					$first_parent = $first_parent_obj->row();
+
+					$priviledges_style .=  "." . $first_parent->name . "{display:block;}";
+
+					if ($first_parent->derivative_id !== 0) {
+
+						$second_parent_obj=$this->db->get_where("entitlement", array("entitlement_id" => $first_parent->derivative_id));
+						if($second_parent_obj->num_rows()>0){
+
+							$second_parent = $second_parent_obj->row();
+
+							$priviledges_style .=  "." . $second_parent->name . "{display:block;}";
+	
+							if ($second_parent->derivative_id !== 0) {
+
+								$third_parent_obj=$this->db->get_where("entitlement", array("entitlement_id" => $second_parent->derivative_id));
+
+								if($third_parent_obj->num_rows()>0){
+
+									$third_parent = $third_parent_obj->row();
+	
+									$priviledges_style .=  "." . $third_parent->name . "{display:block;}";
+		
+									if ($third_parent->derivative_id !== 0) {
+
+										$fourth_parent_obj = $this->db->get_where("entitlement", array("entitlement_id" => $third_parent->derivative_id));
+
+										if($fourth_parent_obj->num_rows()>0){
+
+											$fourth_parent = $fourth_parent_obj->row();
+		
+											$priviledges_style .=  "." . $fourth_parent->name . "{display:block;}";
+
+										}
+
+									
+									}
+
 								}
 							
+							}
+
 						}
+					
+					}
 				}
 			}
-			
-			
 		}
 	endforeach;
-?> 
+
+	echo $priviledges_style;
+	
+	?>
+
 
 </style>
+
